80d1ab4e8dfebd9efeb653949092574ad98e895c
abbie05fuller <abbie.05fuller@gmail.com>
2025-10-31 14:11:21 +0000

    Added JavaDoc to button sections

--- core/src/main/java/io/github/OMAL_Maze/CloseSettingsButton.java
+++ core/src/main/java/io/github/OMAL_Maze/CloseSettingsButton.java
@@ -2,11 +2,17 @@ package io.github.OMAL_Maze;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-
+/**
+ * CloseSettingsButton class extends AbstractButton and inherits all the methods and attributes.
+ * MakeActive and MakeInactive are inherrited and should be used to control when
+ * the buttons are displayed.
+ */
 public class CloseSettingsButton extends AbstractButton{
-    //inherits makeActive and makeInactive from the superclass
 
-
+    /**
+     * Consrtuctor for CloseSettingsButton inheritted from AbstractButton.
+     * @param image the file of the image to represent the button
+     */
     public CloseSettingsButton(FileHandle image){
         super(image);
         super.x = 200;
