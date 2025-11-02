e1be8b8b49fb8cc47a33be6595bfa2d798da9fc0
L-Hannah <154341907+L-Hannah@users.noreply.github.com>
2025-11-02 02:54:39 +0000

    Goose now chases player on spawn

--- core/src/main/java/io/github/OMAL_Maze/Goose.java
+++ core/src/main/java/io/github/OMAL_Maze/Goose.java
@@ -15,6 +15,7 @@ public class Goose extends Character{
     float biteTimer;
     Main instance;
     Rectangle spawnTrigger;
+    Boolean spawned;
     enum gooseState{
         IDLE,
         ANGRY,
@@ -27,17 +28,26 @@ public class Goose extends Character{
         state = gooseState.IDLE;
         //state = gooseState.ANGRY; // for testing, delete later
         this.isMoving = false;
-        this.speed=100f;
+        this.speed=75f;
         this.accelerate=600f;
         this.friction=3000f;
+        this.Xspeed=0f;
+        this.Yspeed=0f;
         this.instance = Main.getInstance();
         this.bitPlayer=false;
         this.createTrigger();
     }
 
     public void show(){
-        visible = true;
-        switch (state) {
+        //"Spawn goose" by moving it to right spawn point
+        this.setPos(11*this.instance.tileSize,14*this.instance.tileSize+16);
+        //Make goose visible
+        this.visible=true;
+        //Start goose moving
+        this.isMoving=true;
+        //Add a boolean to make this only happen once.
+        this.spawned=true;
+        /*switch (state) {
             case IDLE:
                 //Chill
                 break;
@@ -49,7 +59,7 @@ public class Goose extends Character{
                 break;
             default:
                 break;
-        }
+        }*/
     }
 
     public void hide(){
@@ -64,54 +74,17 @@ public class Goose extends Character{
         isMoving = true;
     }
 
-    /*
-    Where the goose moves - in future the code within can be hopefully replaced with calling a Movement class
-    */
+
     @Override
     public void logic(){
         // trying to prevent goose from leaving screen. does not work >
         sprite.setX(MathUtils.clamp(sprite.getX(), 0, instance.viewport.getWorldWidth()-width));
         sprite.setY(MathUtils.clamp(sprite.getY(),0,instance.viewport.getWorldHeight()-height));
-        float playerX = player.sprite.getX();
-        float playerY = player.sprite.getY();
         Rectangle playerBounds = player.sprite.getBoundingRectangle();
-        if (playerBounds.overlaps(this.spawnTrigger)) {
-            //"Spawn goose" by moving it to right spawn point
-            this.sprite.setX(11*this.instance.tileSize);
-            this.sprite.setY(14*this.instance.tileSize+16);
-            //Make goose visible
-            this.visible=true;
-            //Start goose moving
-            //Add a boolean to make this only happen once.
+        if (playerBounds.overlaps(this.spawnTrigger)&&(this.spawned==null || !this.spawned)) {
+            this.show();
         }
 
-        if (isMoving) {
-
-            /* TODO:
-            if (goose collides with player)   {
-                bitePlayer();
-            }
-            */
-
-            final float speed = 1f;
-            float X_diff = playerX - this.x;
-            float Y_diff = playerY - this.y;
-            double distance = Math.sqrt((Math.pow(X_diff, 2) + Math.pow(Y_diff, 2)));
-            double unitVector_x = X_diff / distance;
-            double unitVector_y = Y_diff / distance;
-            sprite.translate((float) (unitVector_x * speed), (float) (unitVector_y * speed));
-//            rectangle.x = sprite.getX();
-//            rectangle.y = sprite.getY();
-        } else if (bitPlayer) {
-            float delta = Gdx.graphics.getDeltaTime();
-            if (this.biteTimer>0f) {
-                this.biteTimer -= delta;
-            } else {
-                this.bitPlayer = false;
-                this.biteTimer = 5000f;
-                this.isMoving=true;
-            }
-        }
     }
 
     /*
@@ -128,10 +101,100 @@ public class Goose extends Character{
     @Override
     public void movement(float delta, Array<Entity> entities, Array<Building> buildings) {
         this.player= Main.player;
+        if (isMoving) {
+
+            /* TODO:
+            if (goose collides with player)   {
+                bitePlayer();
+            }
+            */
+
+            //final float speed = 1f;
+            float X_diff = this.player.sprite.getX() - this.x;
+            float Y_diff = this.player.sprite.getY() - this.y;
+            double distance = Math.sqrt((Math.pow(X_diff, 2) + Math.pow(Y_diff, 2)));
+            if (distance==0) return;
+            double unitVector_x=X_diff/distance;
+            double unitVector_y=Y_diff/distance;
+            if (unitVector_x > 0) {
+                Xspeed+=accelerate*delta;
+                if (Xspeed<0) Xspeed*=0.25f;
+            } else if (unitVector_x<0) {
+                Xspeed-=accelerate*delta;
+                if (Xspeed>0) Xspeed*=0.25f;
+            } else {
+                Xspeed*=Math.max(0,1-friction*delta/speed);
+            }
+            if (unitVector_y > 0) {
+                Yspeed+=accelerate*delta;
+                if (Yspeed<0) Yspeed*=0.25f;
+            } else if (unitVector_y<0) {
+                Yspeed-=accelerate*delta;
+                if (Yspeed>0) Yspeed*=0.25f;
+            } else {
+                Yspeed*=Math.max(0,1-friction*delta/speed);
+            }
+            if (Xspeed>speed)Xspeed=speed;
+            if (Yspeed>speed)Yspeed=speed;
+            if (Xspeed<-speed)Xspeed=-speed;
+            if (Yspeed<-speed)Yspeed=-speed;
+            float moveX=Xspeed*delta;
+            float moveY=Yspeed*delta;
+            this.translate(moveX,0);
+            if (checkOverlaps(entities,buildings)) {
+                this.translate(-moveX,0);
+                Xspeed=0;
+            }
+            this.translate(0,moveY);
+            if (checkOverlaps(entities,buildings)) {
+                this.translate(0,-moveY);
+                Yspeed=0;
+            }
+        } else if (bitPlayer) {
+            if (this.biteTimer>0f) {
+                this.biteTimer -= delta;
+            } else {
+                this.bitPlayer = false;
+                this.biteTimer = 5000f;
+                this.isMoving=true;
+            }
+        }
         this.logic();
     }
 
+    private boolean checkOverlaps(Array<Entity> entities, Array<Building> buildings) {
+        for (int i=0;i< buildings.size;i++) {
+            Building building=buildings.get(i);
+            if (building.Overlaps(this.rectangle)) {
+                return true;
+            }
+        }
+        for (int i=0;i< entities.size;i++) {
+            Entity entity = entities.get(i);
+            if (entity==this) continue;
+            if (!entity.isSolid) continue;
+            if (entity.Overlaps(this.rectangle)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private void setPos(float newX, float newY) {
+        this.x=(int)newX;
+        this.y=(int)newY;
+        this.rectangle.x=newX;
+        this.rectangle.y=newY;
+        this.sprite.setX(newX);
+        this.sprite.setY(newY);
+    }
+    private void translate(float distX, float distY) {
+        this.sprite.translate(distX,distY);
+        this.x=(int)this.sprite.getX();
+        this.y=(int)this.sprite.getY();
+        this.rectangle.x=this.sprite.getX();
+        this.rectangle.y=this.sprite.getY();
+    }
 
     void createTrigger() {
         int leftX = 6;
